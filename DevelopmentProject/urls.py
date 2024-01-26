2048a8b2e8dc066eff1068a82319bc94a13ce151
C1003593 <C1003593@my.shu.ac.uk>
2024-01-26 20:37:27 +0000

    26/01/2024 Created home page.

--- DevelopmentProject/urls.py
+++ DevelopmentProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ContactApplication/', include('ContactApplication.urls')),
+    path('', include('ContactApplication.urls')),
 ]
