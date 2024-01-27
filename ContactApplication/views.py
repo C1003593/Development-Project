1fc0695ced10178170aab08100beecd72d9b942d
C1003593 <C1003593@my.shu.ac.uk>
2024-01-27 23:09:48 +0000

    27/01/2024 Base template made.

--- ContactApplication/views.py
+++ ContactApplication/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'ContactApplication/home.html')
+    return render(request, 'ContactApplication/home.html', {'title':'Welcome'})
 
 
