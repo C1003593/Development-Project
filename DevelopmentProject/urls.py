f84c6ed12f4bb1661072aadf4abfd8d2e1e07b1c
C1003593 <C1003593@my.shu.ac.uk>
2024-02-19 18:54:59 +0000

    Added image field to mentor profiles.

--- DevelopmentProject/urls.py
+++ DevelopmentProject/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from users import views as user_views
 from users import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,6 +43,4 @@ urlpatterns = [
 
     path('mentorrefgen', user_views.MentorNumGen, name = 'mentornumgen'),
 
-
-
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
